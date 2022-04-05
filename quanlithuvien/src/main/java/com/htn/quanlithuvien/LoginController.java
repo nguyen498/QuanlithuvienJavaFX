28abf603064bf150fb023d854c8df005a545661b
nguyen498 <nh77442@gmail.com>
2022-04-05 18:06:09 +0700

    add signup-login-controller

--- quanlithuvien/src/main/java/com/htn/quanlithuvien/LoginController.java
+++ quanlithuvien/src/main/java/com/htn/quanlithuvien/LoginController.java
@@ -4,9 +4,14 @@
  */
 package com.htn.quanlithuvien;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,4 +28,24 @@ public class LoginController implements Initializable {
         // TODO
     }    
     
+    public void loginHandle (ActionEvent evt) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("ManagerGUI.fxml"));
+        
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage ();
+        stage.setScene(scene);
+        stage.setTitle("Manager");
+        stage.show();
+    }
+    
+    public void signUpHandle (ActionEvent evt) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("SignUp.fxml"));
+        
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage ();
+        stage.setScene(scene);
+        stage.setTitle("Sign Up");
+        stage.show();
+    }
+
 }
