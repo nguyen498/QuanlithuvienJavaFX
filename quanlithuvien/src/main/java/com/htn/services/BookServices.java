97fc9cce385b7264516e35425eb63aafdad4333d
onggiabayluon <livipst2@gmail.com>
2022-04-10 15:29:59 +0700

    fix

--- quanlithuvien/src/main/java/com/htn/services/BookServices.java
+++ quanlithuvien/src/main/java/com/htn/services/BookServices.java
@@ -34,18 +34,30 @@ public class BookServices {
            List<Book> books = new ArrayList<>();
            
            while (rs.next()) {
-               int id = rs.getInt("id");
-               String name = rs.getString("name");
-               String description = rs.getString("description");
-               double price = rs.getDouble("price");
-               Date dateOfPurcharse = rs.getDate("dateOfPurcharse");
-               String publicationPlace = rs.getString("publicationPlace");
-               int status = rs.getInt("status");    
-               books.add(new Book(id,name,description ,price ,dateOfPurcharse,publicationPlace, status));
+               Book book = new Book(rs);
+               books.add(book);
            }
         return books;
         }
     }
     
+    public boolean addBook (Book b) throws SQLException{
+        String sql = "INSERT INTO Book(name, description, price, dateOfPurcharse, publicationPlace, status) " + "VALUES (?, ?, ?, ?, ?, ?)";
+        try (Connection conn = JdbcUtils.getConn()) {
+                conn.setAutoCommit(false);
+                PreparedStatement stm = conn.prepareStatement(sql);
+                stm.setString(1, b.getName());
+                stm.setString(2, b.getDescription());
+                stm.setFloat(3,(float) b.getPrice());
+                stm.setDate(4, b.getDateOfPurcharse());
+                stm.setString(5, b.getPublicationPlace());
+                stm.setInt(6, b.getStatus());
+
+                stm.executeUpdate();
+
+                conn.commit();
+        }
+        return true;
+    }
     
 }
--- quanlithuvien/src/main/java/com/htn/utils/Utils.java
+++ quanlithuvien/src/main/java/com/htn/utils/Utils.java
@@ -4,6 +4,11 @@
  */
 package com.htn.utils;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.control.Alert;
 
 /**
@@ -11,10 +16,24 @@ import javafx.scene.control.Alert;
  * @author admin
  */
 public class Utils {
-      public static Alert showBox(String msg, Alert.AlertType type) {
-        Alert a = new Alert(type);
-        a.setContentText(msg);
+    public static Alert showBox(String msg, Alert.AlertType type) {
+      Alert a = new Alert(type);
+      a.setContentText(msg);
+
+      return a;
+    }
+    
+    public static Date toSqlDate(String ntn) {
+        try {
+            java.util.Date utilDate = new SimpleDateFormat("dd/MM/yyyy").parse(ntn);
+            
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+            
+            return sqlDate;
+        } catch (ParseException ex) {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
-        return a;
+        return null;
     }
 }
